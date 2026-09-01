273f9d607d6ce38cfdb51da93f7adeda0d54aa7d
xyryc <amdanikzz@gmail.com>
2026-09-01 15:50:59 +0600

    chore: remove legacy root files after moving to opentaskit-app

--- src/data/images.ts
+++
@@ -1,7 +0,0 @@
-export const IMG = {
-  cleaning: require('../../assets/images/6a841ba1-8fc6-4b01-a607-4adb387cf9da.jpg'),
-  plumbing: require('../../assets/images/8cbdb637-4cc5-4c07-8c25-834788ef6d44.jpg'),
-  moving: require('../../assets/images/d183c680-8ddb-4216-bbea-8cc2609442ab.jpg'),
-  garden: require('../../assets/images/ecb7f488-08b1-4275-b360-9e627ee91dcc.jpg'),
-  ac: require('../../assets/images/df7760d8-efbf-49cc-be92-3aaccc99e481.jpg'),
-};
--- src/store/api/apiSlice.ts
+++
@@ -1,8 +0,0 @@
-﻿import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-export const apiSlice = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
-  tagTypes: ['Task', 'User', 'Offer', 'Review'],
-  endpoints: () => ({}),
-});
