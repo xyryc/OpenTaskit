73cf439eb84c4b8602833422012962cc374f82ef
xyryc <amdanikzz@gmail.com>
2026-09-05 16:44:40 +0600

    feat(backend): configure swagger docs

--- opentaskit-backend/src/main.ts
+++ opentaskit-backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,10 +24,35 @@ async function bootstrap() {
     }),
   );
 
+  // 4. Swagger / OpenAPI Configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('OpenTaskit API')
+    .setDescription(
+      'REST API documentation for OpenTaskit mobile marketplace and admin portal.',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter your JWT access token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(
     `OpenTaskit API server running at http://localhost:${port}/api/v1`,
   );
+  console.log(`Swagger Interactive Docs: http://localhost:${port}/api/docs`);
 }
+
 void bootstrap();
